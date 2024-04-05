bd0126d0d6cb3b1f4ed8a975b51f9ac01f142c0f
doheeing <doheeehan@naver.com>
2024-04-05 22:16:20 +0900

    movie page 만들기

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import MoviePage from "./pages/Movies/MoviePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//홈페이지 /
+// 영화 전체 보여주는 페이지 /movies
+//영화 디테일 보여주는 페이지 /movies/:id
+// 추천영화 /movies/:id/recommadation
+// 리뷰 /movies/:id/reviews
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<AppLayout/>}>
+          <Route index element={<Homepage/>}/>
+          <Route path="movies">
+            <Route index element={<MoviePage/>}/>
+            <Route path=":id" element={<MovieDetailPage/>}/>
+          </Route>
+        </Route>
+    <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NotFoundPage.css'
+
+const NotFoundPage = () => {
+  return (
+    <div className="not-found-page">
+      123
+    </div>
+  )
+}
+
+export default NotFoundPage
